7db332f7fd6639351da2aa51d6476d0f33b638ac
Shivendra Pratap Singh <shivendrapratapsingh@newrelic.com>
2025-05-18 16:52:14 +0530

    testing format changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,18 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended' // Add Prettier integration
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'], // Add Prettier plugin
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'error' // Enforce Prettier rules
   },
 }
--- src/components/Desktop/Desktop.jsx
+++ src/components/Desktop/Desktop.jsx
@@ -3,7 +3,7 @@ import './Desktop.scss'
 export default function Desktop() {
     return <div className="desktop-wrapper">
         {
-            Array(5).fill(0).map((_, index) => <WindowWrapper key={index} view="terminal" />)
+             <WindowWrapper key={index} view="terminal" />
         }
         {/* <WindowWrapper view="terminal" /> */}
     </div>
